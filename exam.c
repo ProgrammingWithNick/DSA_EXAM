1a0feaaf9d525141565532b79d13e709dc3c00f0
Nikhil <167402044+ProgrammingWithNick@users.noreply.github.com>
2025-11-13 23:33:05 +0530

    Add files via upload

---
+++ exam.c
@@ -0,0 +1,11 @@
+153 number
+
+int findMin(int* nums, int numsSize) {
+    n=1;
+    for (int i=n; i<=n; i++) {
+        n==nums.lengh;
+        n=<=n<=10;
+        n=<=nums[i]<= 10;
+    }
+    findMin;
+};
